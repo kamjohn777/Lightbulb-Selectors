b1b31256a6d9aed3e33a08e596827a090c035d09
Johnson <kameron.johnson@hp.com>
2023-07-01 14:53:21 -0500

    added script tag to bottom of html document and updated the javascript script.js file

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Write your code here
+let lightBulb1 = document.querySelector("#lightbulb1");
+let lightBulb2 = document.querySelector("#lightbulb2");
+let lightBulb3 = document.querySelector("#lightbulb3");
+let subtitle = document.querySelector(".subtitle");
+let count = 0;
+
+lightBulb1.addEventListener("click", function() {
+    count++;
+    subtitle.innerHTML = `You've clicked the lightbulb ${count} times`
+    this.classList.toggle("active");
+});
+
+lightBulb2.addEventListener("click", function() {
+    count++;
+    subtitle.innerHTML = `You've clicked the lightbulb ${count} times`
+    this.classList.toggle("active");
+});
+
+lightBulb3.addEventListener("click", function() {
+    count++;
+    subtitle.innerHTML = `You've clicked the lightbulb ${count} times`
+    this.classList.toggle("active");
+});
